47d4dabd755054ea41093c605435465c2a1b04f7
solrun <solalola@gmail.com>
2014-04-08 21:20:37 +0000

    added findBlock func

--- players.py
+++ players.py
@@ -10,11 +10,31 @@ from connect4 import *
         action=random.randint(0,5)
     c4.simulate(action,player)
 """
-def randommc(player,c4,n):
+def randommc(c4,numRollouts):
+	cumreward = [0]*6
 	for action in (col for col in range(6) if c4[6][col]==0):
-		c4temp = c4.copy()
-		for i in range(n):
-			
-			
+		reward = range(numRollouts) 
+		for i in range(numRollouts):
+			c4temp = c4.copy()
+			win = None
+			state,win =c4temp.simulate(action)
+			while win is None:
+				state,win = c4temp.makeRandomMove()
+			if win == 0:
+				r = 0.5
+			else:
+				r = win*c4.currPlayer
+			reward[i]=n
+		cumreward[action] = sum(reward)
+	bestmove=cumreward.index(max(cumreward))
+	c4.simulate(bestmove)
 
-		
+def findBlock(c4):
+for col in range(6):	
+    c4Check = c4.copy()
+    c4Check.currPlayer*=-1
+    state,win = c4Check.simulate(col)
+    if win = -c4.currPlayer:
+        return col
+if __name__=="__main__":
+	c4=
