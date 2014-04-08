f87d5cc5abe64bb4145fa0cc19aaf4650e89c2a5
solrun <solalola@gmail.com>
2014-04-08 21:50:52 +0000

    smartermc

--- players.py
+++ players.py
@@ -30,11 +30,55 @@ def randommc(c4,numRollouts):
 	c4.simulate(bestmove)
 
 def findBlock(c4):
-for col in range(6):	
-    c4Check = c4.copy()
-    c4Check.currPlayer*=-1
-    state,win = c4Check.simulate(col)
-    if win = -c4.currPlayer:
-        return col
+    blockCol = None
+    for col in range(6):	
+        c4Check = c4.copy()
+        c4Check.currPlayer*=-1
+        state,win = c4Check.simulate(col)
+        if win = -c4.currPlayer:
+    return blockCol
+
+def findWin(c4):
+    winCol = None
+    for col in range(6):	
+        c4Check = c4.copy()
+        state,win = c4Check.simulate(col)
+        if win = c4.currPlayer:
+    return winCol
+
+
+def smartermc(c4,numRollouts):
+	winningMove = findWin(c4)
+    blockMove = findBlock(c4)
+    if winningMove != None:
+        bestmove = winningMove
+    elif blockMove != None:
+        bestmove = blockMove
+    else:
+        cumreward = [0]*6 
+        for action in (col for col in range(6) if c4[6][col]==0):
+            reward = range(numRollouts) 
+            for i in range(numRollouts):
+                c4temp = c4.copy()
+                win = None
+                state,win =c4temp.simulate(action)
+                while win is None:
+                    winningMove = findWin(c4)
+                    blockMove = findBlock(c4)
+                    if winningMove != None:
+                        state, win = c4temp.simulate(winningMove)
+                    elif blockMove != None:
+                        state, win = c4temp.simulate(blockMove)
+                    else:
+                        state,win = c4temp.makeRandomMove()
+                if win == 0:
+                    r = 0.5
+                else:
+                    r = win*c4.currPlayer
+                reward[i]=n
+            cumreward[action] = sum(reward)
+        bestmove=cumreward.index(max(cumreward))
+	c4.simulate(bestmove)
+
 if __name__=="__main__":
 	c4=
